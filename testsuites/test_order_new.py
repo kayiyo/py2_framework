b5b9743181de434b326383296519eaf9a5e3b74e
kk <kayiyo@live.cn>
2018-04-08 16:39:21 +0800

    update

--- testsuites/test_order_new.py
+++ testsuites/test_order_new.py
@@ -55,16 +55,6 @@ class OrderNew(unittest.TestCase):
 
         orderbase.get_windows_img()
         orderbase.logout()
-        # homepage = HomePage(self.driver)
-        # homepage.type_search('selenium')  # 调用页面对象中的方法
-        # homepage.send_submit_btn()     #调用页面对象类中的点击搜索按钮方法
-        # time.sleep(2)
-        # homepage.get_windows_img()  # 调用基类截图方法
-        # try:
-        #     assert 'selenium' in homepage.get_page_title()  # 调用页面对象继承基类中的获取页面标题方法
-        #     print ('Test Pass.')
-        # except Exception as e:
-        #     print ('Test Fail.', format(e))
 
 if __name__ == '__main__':
     unittest.main()
